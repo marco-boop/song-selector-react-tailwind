43ca2adab38a90e763838c90dca00422cce3b01b
Marco <sanchez.junco@gmail.com>
2024-01-22 11:56:05 -0800

    tailwind practice in new react app

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dyeindigo': '#023A4E',
+        'caffreepepsi': '#E6B021',
+        'cbpink': '#EF476F',
+        'antiflashwhite': '#EBEBEB',
+      }
+    },
+  },
+  plugins: [],
+}
+
